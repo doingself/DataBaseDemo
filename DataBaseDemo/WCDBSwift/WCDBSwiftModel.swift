6b8032a230fc12da59c211bb3151a3bcc4abb772
史彦超 <daviondk@163.com>
2021-08-25 16:29:52 +0800

    add TemplateModel

--- DataBaseDemo/WCDBSwift/WCDBSwiftModel.swift
+++ DataBaseDemo/WCDBSwift/WCDBSwiftModel.swift
@@ -43,3 +43,60 @@ class WCDBSwiftClassModel: TableCodable {
     }
     
 }
+
+
+/// 模板 model
+struct TemplateModel: TableCodable {
+    
+    var identifier: Int?
+    var description: String?
+    var name: String?
+    
+    enum CodingKeys: String, CodingTableKey {
+        typealias Root = TemplateModel
+        static let objectRelationalMapping = TableBinding(CodingKeys.self)
+        
+        case identifier = "id"
+        case description
+        case name
+        
+        /// 列约束
+        static var columnConstraintBindings: [TemplateModel.CodingKeys : ColumnConstraintBinding]? {
+            return [
+                identifier: ColumnConstraintBinding(
+                    isPrimary:  true, // 是否主键
+                    orderBy: nil, // 主键时, 存储的排序方式
+                    isAutoIncrement: false, // 主键自增
+                    onConflict: nil, // 主键冲突后处理
+                    isNotNull: true, // 是否可空
+                    isUnique: true, // 是否唯一
+                    defaultTo:  nil // 默认值
+                ),
+                name: ColumnConstraintBinding(defaultTo: ColumnDef.DefaultType.text("name"))
+            ]
+        }
+        
+        /// 索引
+        static var indexBindings: [IndexBinding.Subfix : IndexBinding]? {
+            return [
+                "_uniqueIndex": IndexBinding(isUnique: true, indexesBy: identifier)
+            ]
+        }
+        
+        /// 表约束
+        static var tableConstraintBindings: [String : TableConstraintBinding]? {
+            /*
+            MultiPrimaryBinding: 联合主键约束
+            MultiUniqueBinding: 联合唯一约束
+            CheckBinding: 检查约束
+            ForeignKeyBinding: 外键约束
+            */
+            return nil
+        }
+        static var virtualTableBinding: VirtualTableBinding? {
+            return nil
+        }
+        
+    }
+    
+}
--- Podfile
+++ Podfile
@@ -31,6 +31,7 @@ post_install do |installer|
   installer.pods_project.targets.each do |target|
     target.build_configurations.each do |config|
       config.build_settings["EXCLUDED_ARCHS[sdk=iphonesimulator*]"] = "$(inherited) arm64"
+      config.build_settings['ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES'] = '$(inherited)'
     end
   end
 end
